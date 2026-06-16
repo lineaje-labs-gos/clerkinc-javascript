43a4f2c72a1c730487a91ed26eb943023702c6bf
Laura Beatris <laurabeatriserafim@gmail.com>
2026-06-16 14:30:23 -0300

    Add descriptors

--- packages/ui/src/customizables/elementDescriptors.ts
+++ packages/ui/src/customizables/elementDescriptors.ts
@@ -631,6 +631,10 @@ export const APPEARANCE_KEYS = containsAllElementsConfigKeys([
   'configureSSOResetConnectionDialogConfirmationInput',
   'configureSSOResetConnectionDialogSubmitButton',
 
+  'configureSSOChangeProviderDialog',
+  'configureSSOChangeProviderDialogCancelButton',
+  'configureSSOChangeProviderDialogConfirmButton',
+
   'configureSSORemoveDomainDialog',
   'configureSSORemoveDomainDialogCancelButton',
   'configureSSORemoveDomainDialogSubmitButton',
